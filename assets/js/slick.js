a09edbe93745694a745e04590b1e56afd2755d22
George Pyott <george.pyott@googlemail.com>
2019-11-08 16:50:12 +0000

    js for packages slick slider

--- assets/js/slick.js
+++ assets/js/slick.js
@@ -8,3 +8,29 @@ $('.main-body__slick').slick({
   dots: false,
   rows: 0
 });
+
+$('.packages__slick').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    mobileFirst: true,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    dots: false,
+    arrows: false,
+    rows: 0,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 4
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2
+            }
+        },
+        ]
+});
