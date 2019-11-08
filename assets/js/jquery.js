5fe280e726459cef25f7dee36b728776f4d79f36
George Pyott <george.pyott@googlemail.com>
2019-11-08 17:06:56 +0000

    added code for click to close

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -7,3 +7,9 @@ $('.nav-button').on('click', function(e) {
     e.preventDefault();
     $('html').addClass('open-nav show-overlay');
 });
+
+//click to close
+
+$('.overlay').on('click', function(){
+    $('html').removeClass('open-nav show-overlay')
+})
