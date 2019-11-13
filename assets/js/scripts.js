ef8c4652db51511147d99983b578ab5264892ae7
George Pyott <george.pyott@googlemail.com>
2019-11-13 13:40:26 +0000

    added code for showing map markers based on user input

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -13,39 +13,6 @@ function formCheck() {
 // https://developers.google.com/maps/documentation/javascript/adding-a-google-map - documentation
 // https://maps.googleapis.com/maps/api/js?key=AIzaSyDZiJgFymn04HyksKtcoqVf1LlJdY2cAGQ&callback=initMap
 
-// function initMap() {
-
-//     const options = {
-//         zoom: 4,
-//         center: {lat:49.473925,lng:6.988208}
-//     }
-
-//     const map = new google.maps.Map(document.getElementById('map'), options);
-
-//     const xhttp = new XMLHttpRequest();
-//     xhttp.onreadystatechange = function() {
-//         if (this.readyState == 4 && this.status == 200) {
-//            const response = JSON.parse(xhttp.responseText);
-
-//            console.log(response.markers);
-           
-//                 for(var i = 0;i < response.markers.length;i++){
-                    
-//                     var index = response.markers[i];
-
-//                      let marker = new google.maps.Marker({
-//                       position: new google.maps.LatLng(index.lat, index.lng),
-//                       map: map,
-//                       title: index.content
-//                     });
-//                 }            
-//         }
-//     };
-
-//     xhttp.open("GET", "assets/data/markers.json", true);
-//     xhttp.send();
-// }
-
 function initMap() {
 
     const options = {
@@ -70,10 +37,9 @@ function initMap() {
 
                     console.log(dataType);
 
-
-                    for(var i = 0;i < response.dataType;i++){
+                    for(var i = 0;i < response[dataType].length;i++) {
                         
-                        var index = response.dataType[i];
+                        var index = response[dataType][i];
 
                             let marker = new google.maps.Marker({
                             position: new google.maps.LatLng(index.lat, index.lng),
