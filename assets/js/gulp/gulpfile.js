1307913c54257f5c0d3f039e17391fd03b8fe006
George Pyott <george.pyott@googlemail.com>
2019-11-19 21:21:33 +0000

    Updated gulp.js - gulpfile.js (correct file name) + added packages to folder

--- assets/js/gulp/gulpfile.js
+++ assets/js/gulp/gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp');
 
 gulp.task('minify', function(){
-    gulp.src('assets/js/*.js')
+    gulp.src('assets/js/scripts.js')
     .pipe(uglify())
     .pipe(gulp.dest('assets/js'));
-})
+});
