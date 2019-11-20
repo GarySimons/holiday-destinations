d49623a432b7c7ecbf91913b416155cfd63a6413
George Pyott <george.pyott@googlemail.com>
2019-11-20 21:09:41 +0000

    Switched to using fetch() instead of sending XHR request

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -22,11 +22,11 @@ $('.overlay').on('click', function(){
     $('html').removeClass('open-nav show-overlay')
 })
 
-// Google Maps API
-// https://developers.google.com/maps/documentation/javascript/adding-a-google-map - documentation
-// https://maps.googleapis.com/maps/api/js?key=AIzaSyCEwy3EIAwPBRcIbRPSHTUDgyAGRinxBUY&callback=initMap
-
 function initMap() {
+
+    fetch("assets/data/markers.json")
+        .then(response => response.json())
+        .then(response => {
 
     // default load pos for map
 
@@ -37,57 +37,43 @@ function initMap() {
 
     const map = new google.maps.Map(document.getElementById('map'), options);
 
-    // send request to JSON object locally
-    // XMLHttpRequest code from w3schools.com
+        $(".options").on("click", function(e){
+            e.preventDefault();
+            let dataType = $(this).attr('data-options');
 
-    const xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-           const response = JSON.parse(xhttp.responseText);
+            // loop through the object specifically the array that has been selected
 
-           // get data attribute from user input and store it
+            for(let i = 0;i < response[dataType].length;i++) {
 
-            $(".options").on("click", function(e){
-                e.preventDefault();
-                let dataType = $(this).attr('data-options');
+                // grab the current iteration and store it in index
+                
+                let index = response[dataType][i];
 
-                // loop through the object specifically the array that has been selected
+                // construct the map markers
 
-                for(let i = 0;i < response[dataType].length;i++) {
+                    let marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(index.lat, index.lng),
+                    map: map,
+                    animation: google.maps.Animation.DROP,
+                    title: index.title,
+                    content: index.content
+                });
 
-                    // grab the current iteration and store it in index
-                    
-                    let index = response[dataType][i];
+                // if the data has content allow a click to open info
 
-                    // construct the map markers
-
-                        let marker = new google.maps.Marker({
-                        position: new google.maps.LatLng(index.lat, index.lng),
-                        map: map,
-                        animation: google.maps.Animation.DROP,
-                        title: index.title,
-                        content: index.content
+                if (marker.content){
+                    let toolTip = new google.maps.InfoWindow({
+                        content: marker.content,
+                        title: marker.title
                     });
 
-                    // if the data has content allow a click to open info
-
-                    if (marker.content){
-                        let toolTip = new google.maps.InfoWindow({
-                            content: marker.content,
-                            title: marker.title
-                        });
-
-                        marker.addListener('click', function(){
-                            toolTip.open(map, marker);
-                        });
-                    };
-                }; 
-            });
-        };
-    };
-
-    xhttp.open("GET", "assets/data/markers.json", true);
-    xhttp.send();
-
+                    marker.addListener('click', function(){
+                        toolTip.open(map, marker);
+                    });
+                };
+            }; 
+        });
+    });
 };
 
+   
