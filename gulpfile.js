3d4bb05a2b11fac73bcc48318e70b3506e703bdc
George Pyott <george.pyott@googlemail.com>
2019-12-03 21:09:46 +0000

    Added css minify dependancy to package file + included css minify task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 let gulp = require('gulp');
 let uglify = require('gulp-uglify-es').default;
+let cleanCSS = require('gulp-clean-css');
 
 gulp.task("uglify", function(){
     return gulp.src("assets/js/*.js")
         .pipe(uglify())
         .pipe(gulp.dest("assets/js/min_files"))
-})
+});
+
+gulp.task('minify-css', () => {
+  return gulp.src('assets/css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('assets/css/'));
+});
