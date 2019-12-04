59ea35e937cb8875c7e9a673a990d7e2fe029db8
George Pyott <george.pyott@googlemail.com>
2019-12-04 23:37:23 +0000

    Refactored overlay+hidden nav code

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -13,14 +13,12 @@ const fixedHeader = $('.main-header');
     }
   });
 
-//nav open button
-$('.nav-button').on('click', function() {
-    $('html').addClass('open-nav show-overlay');
-});
-
-//click to close
-$('.overlay, .mobile-nav').on('click', function(){
-    $('html').removeClass('open-nav show-overlay')
+$(".overlay, .mobile-nav, .nav-button").click(function() { 
+	if(this.id == "overlay") {
+		$('html').removeClass('open-nav show-overlay')
+	} else {
+		$('html').addClass('open-nav show-overlay');
+	}
 });
 
 optionsNav.on('click', function(){
